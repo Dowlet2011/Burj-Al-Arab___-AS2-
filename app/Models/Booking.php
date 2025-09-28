d962fbe8e4109bd251dd791e70d342bb9c196378
Dowlet2011 <d.atayew2011@gmail.com>
2025-09-28 16:37:14 +0500

    1.2.4

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Booking extends Model
 {
-    //
+    use HasFactory; use Notifiable;
 }
