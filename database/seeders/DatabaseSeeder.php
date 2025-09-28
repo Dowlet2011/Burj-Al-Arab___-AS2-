3a4e6a0c9658e1d8fa60b71ada5123a8b278491a
Dowlet2011 <d.atayew2011@gmail.com>
2025-09-28 16:32:17 +0500

    1.2.2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Booking;
+use App\Models\Guest;
+use App\Models\Hotel;
+use App\Models\Room;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +17,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call(
+            CitySeeder::class,
+        );
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Hotel::factory()->count(10)->create();
+        Room::factory()->count(10)->create();
+        Guest::factory()->count(10)->create();
+        Booking::factory()->count(10)->create();
     }
 }
